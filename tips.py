283c9ff4730cb62905b564ad3e07ebb9f874bfb2
takemi <amk0k2takemi@gmail,com>
2021-08-07 19:50:41 +0900

    210807 add tips.py

---
+++ tips.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import datetime
+
+def current_date():
+    """ 現在の日付を出力する関数
+
+    Args:
+
+    Returns:
+        str: 現在の日付を返す(%Y%m%d)で
+    """
+
+    dt_now = datetime.datetime.now()
+    dt_now_format = dt_now.strftime('%Y%m%d')
+
+    return  dt_now_format
+
+def read_csv(df_name):
+    return pd.read_csv(df_name + ".csv")
+
+def to_csv_date(df, filename):
+    df.to_csv(filename+ '_' + current_date() + '.csv')
+
+
